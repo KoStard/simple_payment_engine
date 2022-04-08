93650839320728248b4fae35fb7013b6e67e9239
KoStard <rub099999720@gmail.com>
2022-04-08 16:34:12 +0400

    Adding small note about the history provider mocking

--- src/transaction_history_provider/transaction_history_provider.rs
+++ src/transaction_history_provider/transaction_history_provider.rs
@@ -10,6 +10,8 @@ use crate::{
  * This trait is supposed to abstract all history providers and many of them will contain network calls or storage reads.
  * So we can expect that in some cases this will include failures that are not related to the transaction/state existance or consistency. 
  * Hence we need to allow the future instances to use these Results. We can also add different types of Errors.
+ * 
+ * automock -> Doesn't support non-static references in the results, hence won't easily work with mocking the results.
  */
 #[automock]
 pub trait TransactionHistoryProvider {
